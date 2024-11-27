ae263f9818415243f475c81857ecff87502ce848
Arina Smirnova <aesmirnova_3@edu.hse.ru>
2024-11-27 22:07:02 +0300

    bot basic interaction with accounts

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-from telebot import types
+import sqlite3
 
 yandex_names = ['yandex', 'Yandex', 'YANDEX',
                 'yandex music', 'yandex.music', 'Yandex music', 'Yandex Music', 'Yandex.Music',
@@ -28,7 +28,7 @@ def start(message):
     keyboard_start = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_add_acc = telebot.types.KeyboardButton(text="Добавить аккаунт")
     keyboard_start.add(button_add_acc)
-    text_start = ("👋 Привет! \n"
+    text_start = (f"👋 Привет, {message.from_user.first_name}! \n"
                   "Я помогу синхронизировать твои плейлисты между платформами. \n"
                   "Для начала, пожалуйста, авторизуйся в сервисах, с которыми ты хочешь работать. \n"
                   "Эта функция всегда доступна по команде /add_acc")
@@ -39,18 +39,54 @@ def add_acc_through_command(message):
     add_acc(message)
 
 @bot.message_handler(content_types=['text'])
-def add_acc_through_text(message):
-    if message.text == "Добавить аккаунт":
+def text_management(message):
+    if message.text == "Добавить аккаунт" or message.text == "Добавить еще аккаунт":
         add_acc(message)
 
-    elif message.text in vk_names:
-        vk_login(message)
-    elif message.text in yandex_names:
-        yandex_login(message)
-    elif message.text in spotify_names:
-        spotify_login(message)
-    elif message.text in zvooq_names:
-        zvooq_login(message)
+    elif message.text == "Готово":
+        keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button_vk = telebot.types.KeyboardButton(text="Плейлисты в VK")
+        button_yandex = telebot.types.KeyboardButton(text="Плейлисты в Яндекс Музыке")
+        button_spotify = telebot.types.KeyboardButton(text="Плейлисты в Spotify")
+        button_zvooq = telebot.types.KeyboardButton(text="Плейлисты в Zvooq")
+        accs = []
+        '''if (есть акк в бд):
+            keyboard.add(button_платформа)
+            accs.append("платформа")'''
+
+        if len(accs) == 0:
+            button_add_acc = telebot.types.KeyboardButton(text="Добавить аккаунт")
+            keyboard.add(button_add_acc)
+            text = ("Дружище, тебя нет подключенных аккаунтов :( \n"
+                    "Авторизуйся на платформах и мы продолжим 💋")
+
+        elif len(accs) == 1:
+            text = (f"Ты привязал 1 аккаунт в сервисе {accs[0]}\n"
+                    "Переходим к выбору плейлиста!")
+
+        else:
+            text = (f"Ты привязал {len(accs)} сервиса \n"
+                    "Выбери платформу, на которой ты хочешь выбрать плейлист")
+
+        bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
+
+    elif message.text in vk_names or message.text in yandex_names or message.text in spotify_names or message.text in zvooq_names:
+        if message.text in vk_names:
+            vk_login(message)
+        if message.text in yandex_names:
+            yandex_login(message)
+        if message.text in spotify_names:
+            spotify_login(message)
+        if message.text in zvooq_names:
+            zvooq_login(message)
+        keyboard_extra_acc = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        button_add_acc = telebot.types.KeyboardButton(text="Добавить еще аккаунт")
+        button_done = telebot.types.KeyboardButton(text="Готово")
+        keyboard_extra_acc.add(button_add_acc, button_done)
+        text_extra_acc = "Ты хочешь авторизоваться где-то еще?"
+        bot.send_message(message.chat.id, text_extra_acc, reply_markup=keyboard_extra_acc)
+
 
     else:
         bot.send_message(message.chat.id, "😔 Дружище, я тебя не понимаю....")
@@ -66,15 +102,20 @@ def add_acc(message):
     bot.send_message(message.chat.id, text_add_acc, reply_markup=keyboard_add_acc)
 
 def vk_login(message):
-    pass
+    bot.send_message(message.chat.id, "😔 Сори, Арина тупая, поэтому я еще не умею логиниться в вк")
+    return
 
 def yandex_login(message):
-    pass
+    bot.send_message(message.chat.id, "😔 Сори, Арина тупая, поэтому я еще не умею логиниться в яндексе")
+    return
 
 def spotify_login(message):
-    pass
+    bot.send_message(message.chat.id, "😔 Сори, Арина тупая, поэтому я еще не умею логиниться в спотике")
+    return
 
 def zvooq_login(message):
-    pass
+    bot.send_message(message.chat.id, "😔 Сори, Арина тупая, поэтому я еще не умею логиниться в звуке \n"
+                                      "(и вообще ты что конченный какой звук кто им вообще пользуется)")
+    return
 
 bot.infinity_polling()
