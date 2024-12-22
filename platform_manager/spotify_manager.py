8f6f8d50c4c196f4e78906195c99591b0c842c4e
trsh2310 <trushkovakt@gmail.com>
2024-12-22 19:48:47 +0300

    FIX manager sp

--- platform_manager/spotify_manager.py
+++ platform_manager/spotify_manager.py
@@ -1,3 +1,5 @@
+import os
+import re
 import requests
 import logging
 from urllib.parse import urlencode
@@ -13,6 +15,13 @@ class SpotifyManager:
 
     AUTH_URL = "https://accounts.spotify.com/authorize"
     API_BASE_URL = "https://api.spotify.com/v1"
+
+    def logout(self):
+        try:
+            os.remove(".spotifycache")
+            print("Successfully logged out.")
+        except FileNotFoundError:
+            print("No cache file found. Already logged out.")
 
     def get_auth_url(self):
         """
@@ -30,61 +39,93 @@ class SpotifyManager:
     def save_token(self, user_id, token, auth_spotify):
         try:
             token_info = auth_spotify.get_access_token(token)
-            return True
+            return token_info
         except SpotifyOauthError:
             return False
 
-    def get_user_playlists(self, user_id):
-        
-        """Получаем список плейлистов пользователя."""
-        
-        token = self.db.get_token(user_id, "spotify")
-        if not token:
-            raise ValueError("Spotify token not found for user.")
 
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
 
-        response = requests.get(f"{self.API_BASE_URL}/me/playlists", headers=headers)
-        if response.status_code != 200:
-            logging.error(f"Failed to fetch playlists: {response.text}")
-            raise ValueError("Failed to fetch playlists.")
 
-        playlists = response.json()["items"]
-        logging.info(f"Fetched {len(playlists)} playlists for user {user_id}.")
+    def get_playlists(self,playlist_name, sp):
+        """Получает доступ ко всем песням из плейлиста в медиатеке пользователя по его названию"""
+        needed_playlist = None
+        user = sp.current_user()
+        current_playlists = sp.user_playlists(user['id'])['items']
 
-        # Формируем список плейлистов с порядковым номером
-        return [{"name": playlist["name"], "id": playlist["id"]} for playlist in playlists]
+        playlist_with_items = []
+        for playlist in current_playlists:
+            if playlist['name'] == playlist_name:
+                needed_playlist = playlist
 
-    def get_playlist_tracks(self, user_id, playlist_id):
-        
-        """Получаем список треков из выбранного плейлиста."""
-        
-        token = self.db.get_token(user_id, "spotify")
-        if not token:
-            raise ValueError("Spotify token not found for user.")
+                break
 
-        headers = {
-            "Authorization": f"Bearer {token}"
-        }
+        for pl in sp.playlist_items(needed_playlist['id'])['items']:
+            sing = ''
+            for i in range(len(pl['track']['artists'])):
+                sing += pl['track']['artists'][i]['name']
+                if i != len(pl['track']['artists']) - 1:
+                    sing += ', '
+            playlist_with_items.append(
+                sing + ' - ' + pl['track']['name'])
+        return playlist_with_items
 
-        response = requests.get(f"{self.API_BASE_URL}/playlists/{playlist_id}/tracks", headers=headers)
-        if response.status_code != 200:
-            logging.error(f"Failed to fetch tracks: {response.text}")
-            raise ValueError("Failed to fetch tracks.")
 
-        tracks = response.json()["items"]
-        logging.info(f"Fetched {len(tracks)} tracks from playlist {playlist_id} for user {user_id}.")
 
-        # Формируем список треков
-        track_list = []
-        for item in tracks:
-            track = item["track"]
-            track_list.append({
-                "track_id": track["id"],
-                "artist": ", ".join([artist["name"] for artist in track["artists"]]),
-                "title": track["name"]
-            })
 
-        return track_list
+
+    def parser(self,link: str) -> str:
+        """Парсит ссылку и извлекает уникальный ID плейлиста"""
+        pattern = r"https://open\.spotify\.com/playlist/([a-zA-Z0-9]+)"
+        match = re.match(pattern, link)
+
+        if match:
+            return match.group(1)
+        else:
+            raise ValueError("Invalid link or playlist ID not found")
+
+
+    def get_playlist_by_url(self,pl_id, sp):
+        """
+
+        :param pl_id: уникальный айди плейлиста, полученный после парсинга ссылки
+        :param sp: объект класса Spootify, через который и производится доступ к аккаунт юзера
+        :return: все песни из плейлиста под уникальным номером pl_id
+        """
+        current_playlist = sp.playlist_items(pl_id)
+        playlist_with_items = []
+        for pl in current_playlist['items']:
+            sing = ''
+            for i in range(len(pl['track']['artists'])):
+                sing += pl['track']['artists'][i]['name']
+                if i != len(pl['track']['artists']) - 1:
+                    sing += ', '
+            playlist_with_items.append(
+                sing + ' - ' + pl['track']['name'])
+        return playlist_with_items
+
+
+    def search_create_add(self, query: list, wanted_name, sp):
+        """
+        Функция ищет песню(а точнее ее уникальный uri код) и добавляет в плейлист с заданным названием
+
+        :param query: список песен на добавление
+        :param wanted_name: имя нового плейлиста
+        :param sp:объект класса Spootify, через который и производится доступ к аккаунт юзера
+        :return: новый плейлист будет добавлен в медиатеку
+        """
+        user_info = sp.current_user()
+        uris = []
+        needed_id = None
+        for i in query:
+            try:
+                ur = sp.search(i)['tracks']['items'][0]['uri']
+                uris.append(ur)
+            except:
+                continue
+        if len(uris) >= 100:
+            uris = uris[:100]
+        sp.user_playlist_create(user_info['id'], name=wanted_name)
+        for playlist in sp.current_user_playlists()['items']:
+            if (playlist['name'] == wanted_name):
+                needed_id = playlist['id']
+        sp.playlist_add_items(playlist_id=needed_id, items=uris)
